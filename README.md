e635e6f822141eae467a65f7d2993fb28d059fcf
Miguel <migueldelcorso@gmail.com>
2020-06-27 23:24:20 -0300

    Probando

---
+++ README.md
@@ -0,0 +1,12 @@
+# Ingenieria de software 2020
+## Trabajo practico N? 3 - Unit testing
+
+**Nombre del grupo: The pythons**
+**Curso: 4K10**
+**Grupo: 1**
+
+### Instalacion
+[Descargar python](https://www.python.org/downloads/)
+```
+python3 -m unittest appTest.py
+```
---
+++ appTest.py
@@ -0,0 +1,15 @@
+# TU 1: Todo producto que se encuentre en la base de datos tendrá que tener un nombre y una descripción, es decir que si por ejemplo tenemos en la base de datos un celular de tal marca, este deberá tener una descripción acorde.
+import unittest
+from app import Product,CreditCard
+
+class testProducts(unittest.TestCase):
+  def testNameAndDescription(self):
+    result=Product('El principito','Libro para niños').validateNameAndDescription()
+    self.assertTrue(result)
+
+# TU 2: A la hora de ingresar el número de tarjeta, el usuario no puede ingresar valores numéricos negativos en ese campo, si lo hace el sistema no permite realizar la compra.
+
+class testPayment(unittest.TestCase):
+  def testCreditCardNumberGreaterThan0(self):
+    result=CreditCard(10).validateCreditCardGreaterThan0()
+    self.assertTrue(result)

663b84ae66f075177ea2c35443a82e5bfba80d0d
Starkloff <starkloffnacho@gmail.com>
2020-06-28 00:43:02 -0300

    PU nacho Agregados

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
 from datetime import date
 
+class User:
+    def __init__(self,nameUser,passwordUser):
+        self.nameUser = nameUser
+        self.passwordUser = passwordUser
+    
+    def setNameUser(self, nameUser):
+        self.nameUser = nameUser
+    
+    def getNameUser(self):
+        return self.nameUser
+
+    def setPasswordUser(self, passwordUser):
+        self.passwordUser = passwordUser
+    
+    def getPasswordUser(self):
+        return self.passwordUser
+    
+    def validateLenghtPasswordUser(self.passwordUser):
+        if (len(self.passwordUser) >= 8):
+            return True
+        else:
+            return False
 
 class Product:
     def __init__(self, name, description):
@@ -40,3 +62,9 @@ class CreditCard:
 
     def validateCreditCardGreaterThan0(self):
         return self.number > 0
+
+    def validateCreditCardOnlyNumber(self.number):
+        if type(self.number) == int:
+            return True
+        else:
+            return False
--- appTest.py
+++ appTest.py
@@ -1,5 +1,5 @@
 import unittest
-from app import Product, CreditCard
+from app import Product, CreditCard, User
 
 
 # TU 1: Todo producto que se encuentre en la base de datos tendrá que tener un nombre y una descripción, es decir que si por ejemplo tenemos en la base de datos un celular de tal marca, este deberá tener una descripción acorde.
@@ -17,4 +17,18 @@ class testProducts(unittest.TestCase):
 class testPayment(unittest.TestCase):
     def testCreditCardNumberGreaterThan0(self):
         result = CreditCard(10).validateCreditCardGreaterThan0()
+        self.assertTrue(result)
+
+# TU 3: A la hora de ingresar el número de tarjeta, el usuario no puede ingresar letras en ese campo, si lo hace el sistema no permite realizar la compra.
+
+class testCreditCardNumber(unittest.TestCase):
+    def testCreditCardNumber(self):
+        result = CreditCard(10).validateCreditCardOnlyNumber()
+        self.assertTrue(result)
+
+# TU 4: La longitud mínima de caracteres de una contraseña debe ser mayor o igual a 8, caso contrario el sistema no permite crear el usuario en el sistema.
+
+class testPasswordLenght(unittest.TestCase):
+    def testPasswordLenght(self):
+        result = User('12345678').validateLenghtPasswordUser()
         self.assertTrue(result)

52ff69d6880811daf77b0bf2eb9926422ea359f5
Miguel <migueldelcorso@gmail.com>
2020-06-27 23:29:09 -0300

    Cambio del readme y format

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Ingenieria de software 2020
 ## Trabajo practico 3 - Unit testing
 
-**Nombre del grupo: The pythons**
-**Curso: 4K10**
 **Grupo: 1**
+
+**Nombre del grupo: The pythons**
+
+**Curso: 4K10**
+
 
 ### Instalacion
 [Descargar python](https://www.python.org/downloads/)
--- app.py
+++ app.py
@@ -1,39 +1,42 @@
-# Product, Cart, CartProduct
-
 from datetime import date
 
+
 class Product:
-  def __init__(self,name,description):
-      self.name = name
-      self.description=description
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
 
-  def setStock(self,stock):
-    self.stock=stock
-  def getStock(self):
-    return self.stock
-  def setPrice(self,price):
-    if price<0:
-      raise ValueError('The price must be greater than 0')
-    self.price=price
-  def getPrice(self):
-    return self.price
+    def setStock(self, stock):
+        self.stock = stock
 
-  def validateNameAndDescription(self):
-    if(len(self.name) and len(self.description)):
-      return True
-    else:
-      return False
+    def getStock(self):
+        return self.stock
+
+    def setPrice(self, price):
+        if price < 0:
+            raise ValueError('The price must be greater than 0')
+        self.price = price
+
+    def getPrice(self):
+        return self.price
+
+    def validateNameAndDescription(self):
+        if(len(self.name) and len(self.description)):
+            return True
+        else:
+            return False
+
 
 class CreditCard:
-  def __init__(self,number):
-    self.number=number
-    self.expiryDate=date.today()
+    def __init__(self, number):
+        self.number = number
+        self.expiryDate = date.today()
 
-  def getExpiryDate(self):
-    return self.expiryDate
+    def getExpiryDate(self):
+        return self.expiryDate
 
-  def getNumber(self):
-    return self.number
+    def getNumber(self):
+        return self.number
 
-  def validateCreditCardGreaterThan0(self):
-    return self.number>0
+    def validateCreditCardGreaterThan0(self):
+        return self.number > 0
